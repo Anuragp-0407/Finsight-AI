97579c00df6e38ca993554f31b6bc21edb45368e
Anuragp-0407 <pandeyanurag0407@gmail.com>
2026-03-14 11:38:36 +0530

    Save remaining changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://finsight-backend-4k3b.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
