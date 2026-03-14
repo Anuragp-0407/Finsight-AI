36820cdb81069936251cffa957d8586de6b903a4
Anuragp-0407 <pandeyanurag0407@gmail.com>
2026-03-14 12:26:15 +0530

    Use enviroment variable for API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://finsight-backend-4k3b.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
